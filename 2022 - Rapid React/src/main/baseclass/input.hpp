5399178a646975e0f336e55707b45488482385a5
KingoftheFlame <81196606+KingoftheFlame@users.noreply.github.com>
2022-01-27 17:50:39 -0600

    Here is the Input class.
    It is kinda pointless and sucks. If you can think of a reason to keep it or a way to make it good let me know.

---
+++ 2022 - Rapid React/src/main/baseclass/input.hpp
@@ -0,0 +1,31 @@
+//This class is unfinished, but it is probably unessasary.
+
+
+class Input{
+
+    public:
+
+    Input(){}
+
+    Input(int port){
+        
+    }
+
+    enum inputs{};//find better name later
+
+    double get(inputs port){
+        switch(port){
+            default: return 0; break;
+        }
+    }
+
+    void setChannel(){
+
+    } 
+
+
+protected:
+    //input device
+    
+
+};
