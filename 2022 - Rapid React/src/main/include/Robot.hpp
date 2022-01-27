4a80afda7ba44561314f26e77527a7baaf32b999
Aleks <89108950+ProgrammerAleks@users.noreply.github.com>
2022-01-26 19:19:06 -0600

    Add files via upload

---
+++ 2022 - Rapid React/src/main/include/Robot.hpp
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <Libraries.hpp>
+#include <Configuration.hpp>
+
+#include <../subsystem/DriveTrain.hpp>
+
+using namespace std;
+
+/*
+The class for the robot
+*/
+class Robot : public frc::TimedRobot {
+public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void TestInit() override;
+  void TestPeriodic() override;
+    
+private:
+  frc::SendableChooser<string> m_chooser;
+  const string kAutoNameDefault = "Default";
+  const string kAutoNameCustom = "My Auto";
+  string m_autoSelected;
+};
