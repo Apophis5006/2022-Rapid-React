1e49c762b1075f340f3accb6b220b34eade8ed5a
Aleks <89108950+ProgrammerAleks@users.noreply.github.com>
2022-02-08 11:09:47 -0600

    Update Joystick.hpp

--- 2022 - Rapid React/src/main/class/input/Joystick.hpp
+++ 2022 - Rapid React/src/main/class/input/Joystick.hpp
@@ -1,41 +1,34 @@
 
-class joystick{
-    protected:
-        frc::Joystick device {0};     
+class Joystick {
+protected:
+    frc::Joystick joystick;
 
-        enum class inputs {Trigger,X,Y,Z,Button,Twist,Throttle,Channel};
+    enum inputs {Trigger,X,Y,Z,Button,Twist,Throttle,Channel};
 
-    public:
-    joystick(int port){
-        device = frc::Joystick(port);
-    }
+public:
+    Joystick(int port = 0) : joystick(port) {}
 
-    double Get(inputs port){
+    double Get(inputs input, int button = 0){
        //find actual values of buttons later
-       switch(port){
-           case inputs::X: return device.GetX(); break;
-           case inputs::Y: return device.GetY(); break;
-           case inputs::Z: return device.GetZ(); break;
-           case inputs::Twist: return device.GetTwist();
-           case inputs::Throttle: return device.GetThrottle(); break;
-           case inputs::Trigger: return device.GetTrigger(); break;
+       switch(input) {
+           case inputs::X: return joystick.GetX(); break;
+           case inputs::Y: return joystick.GetY(); break;
+           case inputs::Z: return joystick.GetZ(); break;
+           case inputs::Twist: return joystick.GetTwist();
+           case inputs::Throttle: return joystick.GetThrottle(); break;
+           case inputs::Trigger: return joystick.GetTrigger(); break;
+           case inputs::Button: return joystick.GetRawButton(button); break;
            default: return -1; break;
        }
     }
-    double Get(inputs port, inputs Channel){
-        switch(Channel){
-            case inputs::X: return device.GetXChannel(); break;
-            case inputs::Y: return device.GetYChannel(); break;
-            case inputs::Z: return device.GetZChannel(); break;
-            case inputs::Throttle: return device.GetThrottleChannel(); break;
-            case inputs::Twist: return device.GetTwistChannel(); break;
+    double GetChannel(inputs Channel){
+        switch(Channel) {
+            case inputs::X: return joystick.GetXChannel(); break;
+            case inputs::Y: return joystick.GetYChannel(); break;
+            case inputs::Z: return joystick.GetZChannel(); break;
+            case inputs::Throttle: return joystick.GetThrottleChannel(); break;
+            case inputs::Twist: return joystick.GetTwistChannel(); break;
             default: return -1.0; break;
         }
     }
-    double Get(inputs port, int numButton){
-        return device.GetRawButton(numButton);
-    }
-
-
-
 };
